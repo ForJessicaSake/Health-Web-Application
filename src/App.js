5b111716429007c7448801c8ed61b80534005b70
jessica joseph <jessicajoseph2019@yahoo.com>
2022-08-29 13:29:06 +0100

    fix: i fixed the bug

--- src/App.js
+++ src/App.js
@@ -10,26 +10,22 @@ import Conditions from './Components/Conditions';
 import Services from './Components/Services';
 import Contact from './Components/Contact';
 
-const stateContext = createContext();
+
 
 function App() {
 
-const {error, loading, data} = UseFetch('https://database')
-
   return (
     <section className="App">
-      <stateContext.Provider value ={{error, loading, data}}>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path= '/about' element = {<About/>}/>
-          <Route path = '/conditions' element = {<Conditions/>}/>
-          <Route path = '/services' element = {<Services/>}/>
-          <Route path = '/contact' element = {<Contact/>}/>
-        </Routes>
-      </Router>
-      </stateContext.Provider>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/conditions' element={<Conditions />} />
+            <Route path='/services' element={<Services />} />
+            <Route path='/contact' element={<Contact />} />
+          </Routes>
+        </Router>
     </section>
   );
 }
--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,16 @@
-import firebase from "firebase/app";
-import "firebase/firestore";
+// import firebase from "firebase/app";
+// import "firebase/firestore";
 
-const firebaseConfig = {
-    apiKey: "AIzaSyDQuOaQ9ROXwiJ32CwJPTh35bSHsCgmO8M",
-    authDomain: "health-f9089.firebaseapp.com",
-    projectId: "health-f9089",
-    storageBucket: "health-f9089.appspot.com",
-    messagingSenderId: "566619518316",
-    appId: "1:566619518316:web:c942434a0b3d53d0fcef49",
-    measurementId: "G-0W678M7DDE",
-};
+// const firebaseConfig = {
+//     apiKey: "AIzaSyDQuOaQ9ROXwiJ32CwJPTh35bSHsCgmO8M",
+//     authDomain: "health-f9089.firebaseapp.com",
+//     projectId: "health-f9089",
+//     storageBucket: "health-f9089.appspot.com",
+//     messagingSenderId: "566619518316",
+//     appId: "1:566619518316:web:c942434a0b3d53d0fcef49",
+//     measurementId: "G-0W678M7DDE",
+// };
 
-firebase.initializeApp(firebaseConfig);
+// firebase.initializeApp(firebaseConfig);
 
-export default firebase;
+// export default firebase;
