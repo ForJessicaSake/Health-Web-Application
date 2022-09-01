476f319554faf30d593555b000a6f72b4883780c
jessica joseph <jessicajoseph2019@yahoo.com>
2022-09-01 03:47:48 +0100

    feat: I added additional data to the services database

--- src/Components/Services.js
+++ src/Components/Services.js
@@ -1,6 +1,5 @@
 import "../Styles/services.css";
 import UseFetch from "./UseFetch";
-import { useState }  from 'react';
 
 function Services() {
   //Importing the data from useFetch(the custom hook)
@@ -9,43 +8,41 @@ function Services() {
   return (
     <section>
       {/* Services hero section */}
-      
+
       <div className="services-home-content">
-          <div className="services-main-content"> 
-              <h3>services we offer <span>at focus eye care</span></h3>
-              <p>Our clinic offers jhiaivabndv  </p>
-              <button className="services-btn">Book a consultation</button>
-           </div>
-           <div>
-              <img className="services-image"  src="" />
-            </div>
+        <div className="services-main-content">
+          <h3>services we offer <span>at focus eye care</span></h3>
+          <p>Our clinic offers jhiaivabndv  </p>
+          <button className="services-btn">Book a consultation</button>
+        </div>
+        <div>
+          <img className="services-image" src="" alt='hero' />
+        </div>
       </div>
 
-      
-      <article className="database">
-        {isPending && <h5>...Loading</h5>}
-        {data.map((article) => (
-          <div className="preview" key={article.id}>
-            <img src={article.image} alt="eye" />
-            <p>{article.title}</p>
-            <p>{article.content}</p>
-          </div>
-        ))}
-      </article>
+        <article className="database">
+          {isPending && <h5>...Loading</h5>}
+          {data.map((article) => (
+            <div className="preview" key={article.id}>
+              <img src={article.image} alt="eye" />
+              <h3>{article.title}</h3>
+              <p>{article.content}</p>
+            </div>
+          ))}
+        </article>
 
-
-    {/* Services bottom section */}
+      {/* Services bottom section */}
       <aside className="personal-service">
         <div className="services-img-cont">
-          <img className="services-bottom-image" src="" alt="eye machine" /> 
+          <img className="services-bottom-image" src="" alt="eye machine" />
         </div>
         <div className="explore-content">
-            <h2>providing personal services</h2>
-            <p>Lorem ipsum</p>
-            <div className="explore">
-              <button className="explore-btn">Explore conditions</button>
-            </div>
-        </div> 
+          <h2>providing personal services</h2>
+          <p>Lorem ipsum</p>
+          <div className="explore">
+            <button className="explore-btn">Explore conditions</button>
+          </div>
+        </div>
       </aside>
 
     </section>
