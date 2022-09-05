506b076ba0d14c3d3ba99216610baa8c7072a492
Juliet <julieto2001@gmail.com>
2022-09-05 23:15:33 +0100

    second commit

--- src/Components/Services.js
+++ src/Components/Services.js
@@ -28,46 +28,7 @@ function Services() {
           <img className="hero-image" src={pic} alt="man pointing" />
         </div>
 
-      </div>
-
-
-      {/* <div className="first-section">
-        <div class="wrapper-first-section">
-            
-            <div class="text1-wrapper">
-                <h1 class="first-text">services we offer <br/>at <span>focus eye</span></h1>
-                <p>
-                Our clinic offers a wide range of private ophthalmologist services.
-                </p>
-                <a href="#" className="btn">Book a Consultation</a>
-                <p className="small-text"><span><FaArrowDown className="arrow"/></span> Explore All Services</p>
-            </div>
-            <div class="img1-wrapper">
-                <img src={pic} alt="laptop" />
-            </div>
-            
-        </div>
-      </div> */}
-
-    {/* <div className="headers">
-      <div class="header-inner container">
-        <div class="header-intro">
-          <h1 class="header-intro-heading">
-            Hi, I'm Juliet. <br /> A Frontend Developer.
-          </h1>
-          <p class="header-intro-lead">
-            I'm skilled at converting web designs from figma and dribble, into functional and responsive websites and web apps.
-          </p>
-          <div class="header-intro-buttons">
-            <a href="https://drive.google.com/file/d/1GN0aatE_eLj7_230Rt2g3AEG29Mn5bQ6/view?usp=drivesdk" class="header-lightgreen-button" target="_blank"><i class="fa fa-download"></i> Download Resume</a>
-          </div>
-        </div>
-
-        <div class="">
-          <img src={pic} class="bg-image" />
-        </div>
-      </div>
-    </div> */}
+      </div>   
 
 
       <article className="database">
