9640c282b8de5472ba3f2310f5a0ce6e205ca8f0
jessica joseph <jessicajoseph2019@yahoo.com>
2022-09-08 04:04:33 +0100

    fix: desktop screen is responsive

--- src/Components/Services.js
+++ src/Components/Services.js
@@ -3,8 +3,8 @@ import UseFetch from "./UseFetch";
 import { Link } from 'react-router-dom'
 import { BsArrowRightCircle } from 'react-icons/bs'
 import { FaArrowDown } from 'react-icons/fa'
-// import pic from "../images/pointing_finger.png";
-// import pics from "../images/eye machine.jpg";
+import pic from "../Images/pointing_finger.png";
+import pics from "../Images/eye machine.jpg";
 
 function Services() {
   //Importing the data from useFetch(the custom hook)
@@ -23,59 +23,58 @@ function Services() {
         </div >
 
 
-    <div>
-      {/* <img className="hero-image" src={pic} alt="man pointing" /> */}
-    </div>
+        <div>
+          <img className="hero-image" src={pic} alt="man pointing" />
+        </div>
       </div >
 
-    {/* Services database section */ }
+      {/* Services database section */}
 
-    <section className = "database" >
-      { isPending && <h5>...</h5>}
-{
-  data.map((article) => (
-    <article className="preview" key={article.id}>
-      <img src={article.image} alt="eye" />
-      <h3>{article.title}</h3>
-      <p>{article.content}</p>
-      <section className="book">
-        <button className="btn"><Link to='/contact'>Book a Consultation</Link></button>
-        <div className="details">
-          <Link to={`/services/${article.id}`}>
-            <span><BsArrowRightCircle className="icon" /></span> <h5>More details</h5>
-          </Link>
-        </div>
-      </section>
-    </article>
-  ))
-}
+      <section className="database" >
+        {isPending && <h5>...</h5>}
+        {data.map((article) => (
+          <article className="preview" key={article.id}>
+            <img src={article.image} alt="eye" />
+            <h3>{article.title}</h3>
+            <p>{article.content}</p>
+            <section className="book">
+              <Link to='/contact'><button className="btn">Book a Consultation </button></Link>
+              <Link to={`/services/${article.id}`}>
+                <div className="details">
+                  <span><BsArrowRightCircle className="icon" /></span> <h5>More details</h5>
+                </div>
+              </Link>
+
+            </section>
+          </article>
+        ))
+        }
       </section >
 
-  {/* Services bottom section */ }
-  <aside className = "personal-services" >
-    <div className="row">
-      <div className="image-cont">
-        {/* <img src={pics} alt="eye machine" /> */}
-      </div>
+      {/* Services bottom section */}
+      <aside className="personal-services">
+        <section className="row">
+          <div className="image-cont">
+            <img src={pics} alt="eye machine" />
+          </div>
 
-      <div className="ps-content">
-        <h3>providing personal service</h3>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-          Debitis, provident beatae, repellat asperiores sequi error esse
-          assumenda quibusdam cumque, alias non earum explicabo sit
-          officiis dignissimos architecto accusantium natus dolore nobis.
-        </p>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-          Debitis, provident beatae, repellat asperiores sequi error esse
-          assumenda quibusdam cumque, alias non earum explicabo sit
-          officiis dignissimos architecto accusantium natus dolore nobis.
-        </p>
-        <Link to="/conditions" className="ps-btn">Explore Conditions</Link>
-      </div>
+          <article className="ps-content">
+            <h3>providing personal service</h3>
+            <p>It is very important for everyone to be healthy. A successful career, relations with relatives and colleagues,
+              solving everyday problems - all this depends on the condition of the body. Illnesses and ailments create serious difficulties
+              which can be avoided with timely examinations and treatment.
+            </p>
+            <p>Dr. Ghaznawi is a member of the American Society of Cataract and Refractive Surgeons. With her extensive training
+              experience, you get accurate diagnosis and treatment that suits your needs. The result is better eyesight and
+              a higher quality of life.
+            </p>
+            <Link to="/conditions" className="ps-btn">Explore Conditions</Link>
+          </article>
+        </section>
 
-    </div>
-      </aside >
-    </section >
+      </aside>
+
+    </section>
   );
 }
 
