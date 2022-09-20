18e782407e8a3a2e9dee72cd04bee36459f0c596
falomo sharon <falomosharl@gmail.com>
2022-09-20 20:00:13 +0100

    first commit

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,5 +1,8 @@
 import '../Styles/home.css'
 import UseFetch from './UseFetch';
+import nurse from '../images/nurse.png'
+import { BsArrowRightCircle } from 'react-icons/bs'
+import { Link } from 'react-router-dom'
 
 function Home() {
     //Importing the data from useFetch(the custom hook)
@@ -9,14 +12,36 @@ function Home() {
         <section>
             <h1> This is the Home section</h1>
             {/* this is how to map through the data from the collection home */}
-            {/* 
-         {isPending && <h5>...Loading</h5>}
-            {data.map((article) => (
-                <div className="preview" key={article.id}>
-                    <p>{article.title}</p>
-                    <p>{article.content}</p>
+
+            <section className="homeDatabase">
+                {isPending && <h5>...Loading</h5>}
+                {data.map((article) => (
+                    <div className="preview" key={article.id}>
+                        <h3>{article.title}</h3>
+                        <p>{article.content}</p>
+                        <div className='learn More'>
+                            <span><BsArrowRightCircle className="icon" /></span> <h5>Learn More</h5>
+                        </div>
+                    </div>
+                ))}
+            </section>
+
+            {/* qualification section */}
+            <section>
+                <div>
+                    <img src={nurse} alt="nurse" />
                 </div>
-            ))} */}
+                <div className='QScontent'>
+                    <h3>OUR QUALIFICATIONS</h3>
+                    <p>Dr. Neelofar Chaznawi is aaa board- certified ophthalmologist and cornea and ecternal disease specialist. Her goal as a practitioner is to keep her patients well informed, and to work with them to develop a treatment plan best suited to their individual needs. Dr. Chaznawi is board certified from the American Board of ophthalmologist and is a member of professional organizations including the American Academy of ophthalmology, the American Society of Cataract and Refractive Surgeons, and Women in Ophthalmology </p>
+                    <section className="book">
+                        <Link to='/contact'><button className="btn">Meet Dr. Chaznawi </button></Link>
+                        <div className="details">
+                            <span><BsArrowRightCircle className="icon" /></span> <h5>Open Certicates</h5>
+                        </div>
+                    </section>
+                </div>
+            </section>
         </section>
     )
 }
